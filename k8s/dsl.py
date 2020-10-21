40ddb822464380c9939c48ac28077878899babc2
OHMORI <kirio9817@gmail.com>
2020-10-21 21:30:43 +0900

    [modify] pipeline
    make pipeline for k8s

--- k8s/dsl.py
+++ k8s/dsl.py
@@ -1,23 +1,32 @@
 import kfp.dsl as dsl
 import kfp.gcp as gcp
 import kfp.compiler as compiler
+from kfp import components
+from kubernetes import client as k8s_client
 
 def git_clone_op(repo_url, password):
     volume_op = dsl.VolumeOp(
         name='create pipeline volume',
         resource_name='pipeline-pvc',
         modes=['ReadWriteMany'],
-        size="30Gi"
+        size="1Gi"
     )
 
     image = 'alpine/git:latest'
 
     commands = [
-        f"git clone {repo_url}",
-        f"cd vq-vae_1d",
-        "apt-get update",
-        "apt-get -y install sshpass",
-        "sshpass -p 'house101' ssh -f k_ohmori@192.168.11.107:~/hdd/ohmori/data ."
+        f"git clone {repo_url} /workspace/vq-vae_1d",
+        # f"cd /vq-vae_1d",
+        # "apk --update add sshpass",
+        # # "apt-get -y install sshpass",
+        # "mkdir ~/.ssh",
+        # # 'echo "Host *" >> ~/.ssh/config',
+        # # 'echo "StrictHostKeyChecking no" >> ~/.ssh/config',
+        # "sshpass -p 'House101' scp -r -o StrictHostKeyChecking=no k_ohmori@192.168.11.107:~/hdd/ohmori/data .",
+        # "rm -rf /var/lib/apt/lists/*",
+        # "rm /var/cache/apk/*",
+        "pwd",
+        'ls',
     ]
 
     # Mount Git repository in /workspace
@@ -44,10 +53,21 @@ def vae_pipeline(data_type='gaze', f_name='vq_vae', num_hiddens=128, num_residua
 
     git_clone = git_clone_op(repo_url=repo_url, password='house101')
 
+    # commands = [
+    #     "ls",
+    #     'cd /home/jovyan/vq-vae_1d/data',
+    #     "ls",
+    #     "cd /home/jovyan/vq-vae_1d",
+    #     "pwd",
+    #     "ls"
+    # ]
+
     train=dsl.ContainerOp(
         name='VQ-VAE',
-        image='zaku.sys.es.osaka-u.ac.jp:10081/OHMORI/vq-vae_1d:ohmori',
-        command=['python3', '~/workspace/vq-vae_1d/script/train.py'],
+        image='zaku.sys.es.osaka-u.ac.jp:10081/ohmori/vq-vae_1d:v0.0.0',
+        # command=['sh'],
+        # arguments=['-c', ' && '.join(commands)],
+        command=['python3', 'train.py'],
         arguments=[
             '--data_type', data_type,
             '--f_name', f_name,
@@ -62,11 +82,29 @@ def vae_pipeline(data_type='gaze', f_name='vq_vae', num_hiddens=128, num_residua
             '--lr', lr,
             '--batch_size', batch_size
         ],
-        pvolumes={'/workspace': git_clone.pvolume},
+        pvolumes={'/home/jovyan': git_clone.pvolume},
         file_outputs={
             'MLPipeline UI metadata': '/mlpipeline-ui-metadata.json'
         }
     )
+
+    train.add_pvolumes({'/workspace': git_clone.pvolume})
+    # train.add_volume_mount(volume_mount=k8s_client.V1VolumeMount(mount_path='/home/jovyan',name=git_clone.name))
+
+    nfs_volume_source = k8s_client.V1NFSVolumeSource(server="192.168.11.107", path="/data/share/data")
+    train.add_volume(k8s_client.V1Volume(name='data', nfs=nfs_volume_source))
+    train.add_volume_mount(k8s_client.V1VolumeMount(mount_path='/home/jovyan/vq-vae_1d/data',name='data'))
+
+    nfs_volume_source = k8s_client.V1NFSVolumeSource(server="192.168.11.107", path="/data/share/pth")
+    train.add_volume(k8s_client.V1Volume(name='pth', nfs=nfs_volume_source))
+    train.add_volume_mount(k8s_client.V1VolumeMount(mount_path='/home/jovyan/vq-vae_1d/script/pth',name='pth'))
+
+    nfs_volume_source = k8s_client.V1NFSVolumeSource(server="192.168.11.107", path="/data/share/logs")
+    train.add_volume(k8s_client.V1Volume(name='logs', nfs=nfs_volume_source))
+    train.add_volume_mount(k8s_client.V1VolumeMount(mount_path='/home/jovyan/vq-vae_1d/script/logs',name='logs'))
+
+    train.container.working_dir = "/home/jovyan/vq-vae_1d/script"
+
 
 if __name__ == '__main__':
     compiler.Compiler().compile(vae_pipeline, 'vq-vae-pipeline.zip')
