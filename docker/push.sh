e02ca972220711afd768f6ae65fb2d668ecab4f2
OHMORI <kirio9817@gmail.com>
2020-11-01 19:41:10 +0900

    [modify] docker image tag name
    -mmodify docker image tag

--- docker/push.sh
+++ docker/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-IMAGE_NAME="zaku.sys.es.osaka-u.ac.jp:10081/ohmori/vq-vae_1d:v0.0.0"
+IMAGE_NAME="zaku.sys.es.osaka-u.ac.jp:10081/ohmori/vq-vae_1d:v0.0.1"
 docker push ${IMAGE_NAME}
--- docker/run2.sh
+++ docker/run2.sh
@@ -1,4 +1,4 @@
-IMAGE_NAME="zaku.sys.es.osaka-u.ac.jp:10081/ohmori/vq-vae_1d:v0.0.0"
+IMAGE_NAME="zaku.sys.es.osaka-u.ac.jp:10081/ohmori/vq-vae_1d:v0.0.1"
 
 docker run --rm -it \
     --privileged \
